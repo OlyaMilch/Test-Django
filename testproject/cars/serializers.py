69b1a34a2460c98098b5432a44572fd29ab797cf
Olya Milch <323234214213m@gmail.com>
2025-09-11 18:18:43 +0300

    Create serializers

---
+++ testproject/cars/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import Country, Manufacturer, Car, Comment
+
+
+# Models are converted to JSON and back
+class CountrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = '__all__'
+
+
+class ManufacturerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Manufacturer
+        fields = '__all__'
+
+
+class CarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Car
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
